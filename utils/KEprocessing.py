e220f1bb74cf07226a5d3293ddd97dd1fd07200f
Oxana Lundstrom <oxana.nu@gmail.com>
2024-02-28 11:42:37 +0100

    Added a function prepare_final_df to use in different notebooks, renamed KE_PCA

--- utils/KEprocessing.py
+++ utils/KEprocessing.py
@@ -29,3 +29,50 @@ def consolidate_data(filenames, fixed_encoding=True):
     combined_df['DATE'] = pd.to_datetime(combined_df['DATE'])
 
     return combined_df
+
+def replace_invalid_with_row_mean(df, residential_threshold, commercial_threshold, min_non_nan=3):
+    # Operate only on the hour columns
+    hour_columns = [f'HOUR_{i}' for i in range(24)]
+    
+    # Filter out rows that don't meet the threshold for non-NaN values
+    df_filtered = df.dropna(thresh=min_non_nan, subset=hour_columns)
+    
+    # Determine thresholds based on customer type
+    thresholds = df_filtered['ISPRIVATEPERSON'].map(lambda x: residential_threshold if x == 'Ja' else commercial_threshold)
+    
+    # Mask the values that are either above their respective thresholds or negative
+    df_filtered[hour_columns] = df_filtered[hour_columns].mask((df_filtered[hour_columns] < 0) | (df_filtered[hour_columns].gt(thresholds, axis=0)))
+    
+    # Calculate the mean for each row excluding NaNs
+    row_means = df_filtered[hour_columns].mean(axis=1)
+    
+    # Replace NaNs with the row's mean
+    df_filtered[hour_columns] = df_filtered[hour_columns].apply(lambda x: x.fillna(row_means[x.name]), axis=1)
+    
+    return df_filtered
+
+
+def prepare_final_df():
+    filenames = ['data/lnu_2020.csv', 'data/lnu_2021.csv', 'data/lnu_2022.csv', 'data/lnu_2023.csv']
+    combined_df = consolidate_data(filenames)
+
+    # Define the hourly columns
+    hourly_columns = [f'HOUR_{i}' for i in range(24)]
+
+    # Calculate the sum of the hourly consumption for each record and create a new column 'One_Day_Power'
+    combined_df['One_Day_Power'] = combined_df[hourly_columns].sum(axis=1)
+     # Calculate the sum of the hourly consumption being NaN and save to a new column
+    combined_df['One_Day_Power_NaN'] = combined_df[hourly_columns].isna().sum(axis=1)
+    
+    # Define your thresholds
+    residential_threshold = 0.03  # 0.03 MWh for residential
+    commercial_threshold = 3      # 3 MWh for commercial
+
+    # Define the minimum number of non-NaN hourly values required to keep a row
+    min_non_nan_values = 3
+
+    combined_df_noNA = replace_invalid_with_row_mean(combined_df, residential_threshold, commercial_threshold, min_non_nan_values)
+    combined_df_noNA.to_csv('data/combined_df_noNA.csv')
+    
+    return(combined_df_noNA)
+    
